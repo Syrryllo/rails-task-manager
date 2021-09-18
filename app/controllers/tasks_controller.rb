256e400ea7b3357e77d951a29dc09c09d8667fc4
Cyril Aulagnon <syrryllo@gmail.com>
2021-09-18 17:51:31 +0200

    ending this day first exercice

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,27 +2,43 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
-    # raise
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    task = Task.create(task_params)
+    task.save
+    redirect_to root_path
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path
   end
 
   private
 
-  def tak_params
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 
 end
